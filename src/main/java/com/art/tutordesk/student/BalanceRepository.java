614789c278ec518cc177995fff437b957585db0c
art-sov <artsovalov@gmail.com>
2025-12-12 15:03:57 +0100

    remove empty rows

--- src/main/java/com/art/tutordesk/student/BalanceRepository.java
+++ src/main/java/com/art/tutordesk/student/BalanceRepository.java
@@ -1,41 +1,25 @@
 package com.art.tutordesk.student;
 
 import com.art.tutordesk.payment.Currency;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import org.springframework.data.jpa.repository.Modifying;
-
 import org.springframework.data.jpa.repository.Query;
-
 import org.springframework.data.repository.query.Param;
-
 import org.springframework.stereotype.Repository;
 
-
-
 import java.util.List;
-
 import java.util.Optional;
 
-
-
 @Repository
-
 public interface BalanceRepository extends JpaRepository<Balance, Long> {
 
     Optional<Balance> findByStudentIdAndCurrency(Long studentId, Currency currency);
 
     List<Balance> findByStudentId(Long studentId);
 
-
-
     @Modifying
-
     @Query("DELETE FROM Balance b WHERE b.student.id = :studentId")
-
     void deleteAllByStudentId(@Param("studentId") Long studentId);
-
 }
 
 
