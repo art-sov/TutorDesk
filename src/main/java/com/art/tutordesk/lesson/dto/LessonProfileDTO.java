caa3ca33c0dbae233b0a69b03726f397c2742ac8
art-sov <artsovalov@gmail.com>
2025-12-17 22:53:53 +0100

    fix pre-filling date and time in edit lesson

--- src/main/java/com/art/tutordesk/lesson/dto/LessonProfileDTO.java
+++ src/main/java/com/art/tutordesk/lesson/dto/LessonProfileDTO.java
@@ -5,6 +5,8 @@ import com.art.tutordesk.lesson.PaymentStatus;
 import lombok.Data;
 import lombok.ToString;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Set;
@@ -12,7 +14,9 @@ import java.util.Set;
 @Data
 public class LessonProfileDTO {
     private Long id;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate lessonDate;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
     private LocalTime startTime;
     private String topic;
     @ToString.Exclude
