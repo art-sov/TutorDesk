ddb7b40a67a2de2e3b531e227b62c84b6eafbc6b
art-sov <artsovalov@gmail.com>
2025-12-18 11:12:22 +0100

    add HomeControllerTest

--- src/main/java/com/art/tutordesk/TutorDeskApplication.java
+++ src/main/java/com/art/tutordesk/TutorDeskApplication.java
@@ -2,10 +2,8 @@ package com.art.tutordesk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class TutorDeskApplication {
 
 	static void main(String[] args) {
