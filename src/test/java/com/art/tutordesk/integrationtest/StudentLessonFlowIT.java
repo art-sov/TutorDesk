8746ccc85181f4c97f06104ecd39445d9a403708
art-sov <artsovalov@gmail.com>
2026-05-16 22:05:08 +0200

    remove unused import

--- src/test/java/com/art/tutordesk/integrationtest/StudentLessonFlowIT.java
+++ src/test/java/com/art/tutordesk/integrationtest/StudentLessonFlowIT.java
@@ -17,7 +17,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
